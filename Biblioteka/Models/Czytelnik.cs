9810231bac0ecf2a80c53cb5bfa9cec56c7a5dc5
bkosmala <b.kosmala@interia.pl>
2016-08-10 13:10:02 +0200

    Tworzenie struktury
    Tworzenie struktury projektu

---
+++ Biblioteka/Models/Czytelnik.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Czytelnik
+    {
+        public int Id_czyt { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Miejscowosc { get; set; }
+        public string Adres { get; set; }
+        public decimal Pesel { get; set; }
+        public string Telefon { get; set; }
+
+    }
+}
---
+++ Biblioteka/Models/Kategoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataOperationsLibrary.Data
+{
+    class Kategoria
+    { 
+    public int Id_kategoria { get; set; }
+    public string Nazwa { get; set; }
+    public string Opis { get; set; }
+    }
+}
