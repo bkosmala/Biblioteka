cda58066f155dc33222261f62c14c58ac6c73406
biedro94 <biedrzyckid94@gmail.com>
2016-08-12 12:57:39 +0200

    Stworzenie Iposition oraz DetailsAboutPosition
    Stworzenie interfejsu IPosition oraz Modelu dotyczacego szczegolu
    pozycji

---
+++ Biblioteka-Bogdan/Biblioteka-Bogdan/Biblioteka/Services/Interfaces/IPosition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using Models.Database;
+using Models.Result;
+
+namespace Services.Interfaces
+{
+    public interface IPosition
+    {
+        Result<List<Pozycja>> Poz { get; }
+        Result<List<Pozycja>> GetPositionForCategory(string nazwaKategorii);
+        Result<List<Pozycja>> GetDetailsForCategory(int idPoz);
+        Result<bool> GetAvailableForPosition(int idPoz);
+    }
+}
