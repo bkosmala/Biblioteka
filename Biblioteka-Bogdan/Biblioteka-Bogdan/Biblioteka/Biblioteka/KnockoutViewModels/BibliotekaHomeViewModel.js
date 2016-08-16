334c421a2c4d9fc40e7f23fb1c4a2f8ba01e42e0
bkosmala <b.kosmala@interia.pl>
2016-08-16 11:07:41 +0200

    poprawka

--- Biblioteka-Bogdan/Biblioteka-Bogdan/Biblioteka/Biblioteka/KnockoutViewModels/BibliotekaHomeViewModel.js
+++ Biblioteka-Bogdan/Biblioteka-Bogdan/Biblioteka/Biblioteka/KnockoutViewModels/BibliotekaHomeViewModel.js
@@ -1,1 +1,5 @@
 ﻿
+/// <reference path="C:\Users\Bogdan.Kosmala\Desktop\Biblioteka\Biblioteka\Biblioteka-Bogdan\Biblioteka-Bogdan\Biblioteka\Biblioteka\Scripts/knockout-3.4.0.js" />
+
+var bibliotekaHomeViewModel;
+
